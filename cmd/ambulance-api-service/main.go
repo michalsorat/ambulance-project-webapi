d3d92c53fad6d2f4b019aaf6f066abab275a001f
michalsorat <michalsorat@gmail.com>
2024-05-27 02:18:25 +0200

    Kostra servera

--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -6,6 +6,7 @@ import (
     "strings"
     "github.com/gin-gonic/gin"
     "github.com/michalsorat/ambulance-project-webapi/api"
+    "github.com/michalsorat/ambulance-project-webapi/internal/ambulance_project"
 )
 
 func main() {
@@ -21,6 +22,7 @@ func main() {
     engine := gin.New()
     engine.Use(gin.Recovery())
     // request routings
+    ambulance_project.AddRoutes(engine)
     engine.GET("/openapi", api.HandleOpenApi)
     engine.Run(":" + port)
 }
---
+++ internal/ambulance_project/impl_ambulance_record_list.go
@@ -0,0 +1,32 @@
+package ambulance_project
+
+import (
+  "net/http"
+
+  "github.com/gin-gonic/gin"
+)
+
+// CreateMealOrder - Creates a new meal order
+func (this *implMealOrdersAPI) CreateMealOrder(ctx *gin.Context) {
+ 	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
+
+// DeleteMealOrder - Deletes a specific meal order
+func (this *implMealOrdersAPI) DeleteMealOrder(ctx *gin.Context) {
+ 	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
+
+// GetMealOrder - Provides details about a specific meal order
+func (this *implMealOrdersAPI) GetMealOrder(ctx *gin.Context) {
+ 	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
+
+// GetMealOrders - Provides the list of meal orders
+func (this *implMealOrdersAPI) GetMealOrders(ctx *gin.Context) {
+ 	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
+
+// UpdateMealOrder - Updates a specific meal order
+func (this *implMealOrdersAPI) UpdateMealOrder(ctx *gin.Context) {
+ 	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
